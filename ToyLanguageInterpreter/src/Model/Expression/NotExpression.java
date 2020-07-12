9c7fe33460916b14eafdf7ae412f1f9a13e3d99d
pop-catalin <catalinpop98@gmail.com>
2020-07-12 16:42:07 +0300

    Add Toy Language Interpreter

---
+++ ToyLanguageInterpreter/src/Model/Expression/NotExpression.java
@@ -0,0 +1,25 @@
+package Model.Expression;
+
+import Model.Utils.IDictionary;
+import Model.Utils.IHeap;
+
+public class NotExpression extends Expression {
+    private Expression expression;
+
+    public NotExpression(Expression expression){
+        this.expression = expression;
+    }
+
+    @Override
+    public Integer evaluate(IDictionary<String, Integer> symbolTable, IHeap heapTable){
+        if (expression.evaluate(symbolTable, heapTable) == 0)
+            return 1;
+        else
+            return 0;
+    }
+
+    @Override
+    public String toString(){
+        return "!" + expression.toString();
+    }
+}
